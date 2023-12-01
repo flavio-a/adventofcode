0e4e143556c310d4ec9361a089fb5077214dfa65
Flavio <flavio.a.618@gmail.com>
2023-12-01 09:49:31 +0100

    Day 1 (couldn't think of a name for the project, maybe I should just ask chatGPT)

---
+++ 2023/crustofcode/src/bin/day1.rs
@@ -0,0 +1,68 @@
+use crustofcode;
+
+fn fl_digits(s: &String) -> u32 {
+    let fst = s
+        .chars()
+        .find(|&c| c.is_digit(10))
+        .unwrap()
+        .to_digit(10)
+        .unwrap();
+    let lst = s
+        .chars()
+        .rfind(|&c| c.is_digit(10))
+        .unwrap()
+        .to_digit(10)
+        .unwrap();
+    return fst * 10 + lst;
+}
+
+fn get_first_digit(s: &String, dir: bool) -> u32 {
+    let mut i = if dir { 0 } else { s.len() - 1 };
+    while if dir { i < s.len() } else { i >= 0 } {
+        let si: char = s.chars().nth(i).unwrap();
+        if si.is_digit(10) {
+            return si.to_digit(10).unwrap();
+        }
+        let slice = &s[i..];
+        if slice.starts_with("one") {
+            return 1;
+        } else if slice.starts_with("two") {
+            return 2;
+        } else if slice.starts_with("three") {
+            return 3;
+        } else if slice.starts_with("four") {
+            return 4;
+        } else if slice.starts_with("five") {
+            return 5;
+        } else if slice.starts_with("six") {
+            return 6;
+        } else if slice.starts_with("seven") {
+            return 7;
+        } else if slice.starts_with("eight") {
+            return 8;
+        } else if slice.starts_with("nine") {
+            return 9;
+        }
+        if dir {
+            i = i + 1;
+        } else {
+            i = i - 1;
+        }
+    }
+    panic!("Fuck");
+}
+
+fn fl_digits_ma_merda(s: &String) -> u32 {
+    return 10 * get_first_digit(s, true) + get_first_digit(s, false);
+}
+
+fn main() {
+    println!("Day 1");
+
+    let lines: Vec<String> = crustofcode::read_input_lines();
+
+    // Part 1
+    println!("{}", lines.iter().map(fl_digits).sum::<u32>());
+    // Part 2
+    println!("{}", lines.iter().map(fl_digits_ma_merda).sum::<u32>());
+}
---
+++ 2023/crustofcode/src/lib.rs
@@ -0,0 +1,141 @@
+use itertools::Itertools;
+use std::fs;
+
+pub type Point = (i64, i64);
+pub type UPoint = (usize, usize);
+
+pub fn read_input() -> String {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Error: missing filename");
+        std::process::exit(-1);
+    }
+
+    let filename = &args[1];
+    // println!("Input: {}", filename);
+
+    fs::read_to_string(filename).expect("Error while reading input file")
+}
+
+pub fn str2int<S: AsRef<str>>(s: S) -> i64 {
+    s.as_ref()
+        .parse()
+        .expect("String parsable to number expected")
+}
+
+pub fn lines_owned(s: String) -> Vec<String> {
+    s.lines().map(|s| s.to_owned()).collect()
+}
+
+/// Read an input file (specified as the first CLI argument) and returns a
+/// vector with its lines (owned)
+pub fn read_input_lines() -> Vec<String> {
+    lines_owned(read_input())
+}
+
+/// Parse a string as an integer per line, and returns the resulting vector.
+///
+/// # Example
+/// ```
+/// use adventofcrustacean::lines_to_ints;
+///
+/// let content = String::from("1\n2\n42");
+/// let nums = lines_to_ints(content);
+///
+/// assert_eq!(nums, [1, 2, 42]);
+/// ```
+pub fn lines_to_ints(content: String) -> Vec<i64> {
+    content.lines().map(str2int).collect()
+}
+
+/// Visualize a vector of points in the 2D space, representing points as the
+/// attached character
+pub fn visualize_as<I>(points: I)
+where
+    I: Iterator<Item = (Point, char)>,
+{
+    let sorted: Vec<(Point, char)> = points
+        .sorted_by(|((x1, y1), _), ((x2, y2), _)| y1.cmp(y2).then(x1.cmp(x2)))
+        .dedup_by(|(p1, _), (p2, _)| p1 == p2)
+        .collect();
+    let maxx: i64 = *sorted.iter().map(|((x, _), _)| x).max().unwrap();
+    let maxy: i64 = *sorted.iter().map(|((_, y), _)| y).max().unwrap();
+
+    let mut x = 0;
+    let mut y = 0;
+    let mut i = 0;
+    while x <= maxx && y <= maxy && i < sorted.len() {
+        if sorted.get(i).unwrap().0 == (x, y) {
+            print!("{}", sorted.get(i).unwrap().1);
+            i += 1;
+        } else {
+            print!(" ");
+        }
+        x += 1;
+        if x > maxx {
+            println!("");
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+/// Visualize a vector of points in the 2D space as █
+pub fn visualize<'a, I>(points: I)
+where
+    I: Iterator<Item = &'a Point>,
+{
+    visualize_as(points.map(|&p| (p, '█')));
+}
+
+/// Visualize a grid of bools
+pub fn visualize_grid(points: &Vec<Vec<bool>>) {
+    for line in points.iter() {
+        for &dot in line.iter() {
+            if dot {
+                // print!("█");
+                print!("#");
+            } else {
+                print!(" ");
+                // print!(".");
+            }
+        }
+        println!("");
+    }
+}
+
+pub fn drop_prefix<'a>(s: &'a str, prefix: &str) -> &'a str {
+    let l = prefix.chars().count();
+    assert_eq!(&s[0..l], prefix.to_string());
+    return &s[l..];
+}
+
+pub fn fst<T, U>(&(x, _): &(T, U)) -> T
+where
+    T: Copy,
+{
+    x
+}
+
+pub fn into_fst<T, U>((x, _): (T, U)) -> T {
+    x
+}
+
+pub fn snd<T, U>(&(_, x): &(T, U)) -> U
+where
+    U: Copy,
+{
+    x
+}
+
+pub fn into_snd<T, U>((_, x): (T, U)) -> U {
+    x
+}
+
+pub fn point_transpose<T>((x, y): (T, T)) -> (T, T) {
+    (y, x)
+}
+
+pub fn up2p((x, y): UPoint) -> Point {
+    (x.try_into().unwrap(), y.try_into().unwrap())
+}
